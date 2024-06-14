aaea07637d676e630b7f3b797edd020c3133dff7
Rene Ponce <11008893@uvu.edu>
2024-06-14 16:43:32 -0600

    player movment and player camera movment
    player movement for the dummy

---
+++ unit 6 lab/Assets/PlayerCamera.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+  public float sensX;
+  public float sensY;
+  float xRotation; 
+  float yRotation;
+
+  public Transform orientation;
+
+  private void Start()
+  {
+    Cursor.lockState = CursorLockMode.Locked;
+    Cursor.visible = false;
+  }
+
+  private void Update()
+  {
+    //get mouse input
+    float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+    float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+
+    yRotation += mouseX;
+    xRotation -= mouseY;
+    xRotation = Mathf.Clamp(xRotation, -9f, 90f);
+    
+    //rotate cam and orientation
+    transform.rotation = Quaternion.Euler(xRotation, yRotation,0);
+    orientation.rotation = Quaternion.Euler(0,yRotation,0);
+    
+  }
+}
--- unit 6 lab/Assets/PlayerMovment.cs
+++ unit 6 lab/Assets/PlayerMovment.cs
@@ -1,25 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovment : MonoBehaviour
 {
-    public Rigidbody rb;
-    [SerializeField] public float moveSpeed = 5f;
+    public float speed = 5f;
+    private float horizotalInput;
+    private float forwardInput;
+    public float jumpHeight = 5f;
+    
+    private Rigidbody playerRB;
+    public bool isOnGround = true;
 
-    [SerializeField] public float Jumping = 5f;
-    // Start is called before the first frame update
+    public KeyCode sprintKey = KeyCode.LeftShift;
+    public KeyCode crouchKey = KeyCode.LeftControl;
+    
+    public float crouchSpeed;
+    public float crouchYscale;
+    private float startYscale;
+    
     void Start()
     {
-        if (Input.GetKey("w"))
-        {
-            rb.AddForce(0,0,moveSpeed * Time.deltaTime);
-        }
+        playerRB = GetComponent<Rigidbody>();
+
+        startYscale = transform.localScale.y;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // gut player input
+        horizotalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
         
+        //move player
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Translate(Vector3.right * Time.deltaTime * speed * horizotalInput);
+
+       
+        
+        //let player jump
+        if (Input.GetKeyDown(KeyCode.Space)&& isOnGround) 
+        {
+            playerRB.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
+            isOnGround = false;
+        }
+        //start crouch
+        if (Input.GetKeyDown(crouchKey))
+        {
+            transform.localScale = new Vector3(transform.localScale.x, crouchYscale, transform.localScale.z);
+            playerRB.AddForce(Vector3.down * 5f, ForceMode.Impulse);
+            
+            //stop crouching
+            if (Input.GetKeyUp(crouchKey))
+            {
+                transform.localScale = new Vector3(transform.localScale.x, startYscale, transform.localScale.z);
+                
+            }
+            //sprint
+        }
     }
 }
