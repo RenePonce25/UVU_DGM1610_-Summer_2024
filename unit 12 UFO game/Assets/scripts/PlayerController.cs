a70e503d1d3a58b850bd10e19c6304ecf0417dd6
Rene Ponce <11008893@uvu.edu>
2024-07-23 13:13:10 -0600

    Unit 12 Lab complete

---
+++ unit 12 UFO game/Assets/scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10f;
+    private float move;
+    private float xlimit = 40f;
+    private Rigidbody playerRb;
+
+    public Transform fireCannon;
+    public GameObject projectile;
+  
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * move* Time.deltaTime * speed);
+
+        if (transform.position.x < -xlimit)
+        {
+            transform.position = new Vector3(-xlimit, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xlimit)
+        {
+            transform.position = new Vector3(xlimit, transform.position.y, transform.position.z);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            Instantiate(projectile, fireCannon.transform.position, projectile.transform.rotation);
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+}
---
+++ unit 12 UFO game/Assets/scripts/Shooting.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public float projectileSpeed = 20;
+    public float zLimit = 30f;
+    
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * projectileSpeed);
+
+        if (transform.position.z < zLimit)
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
